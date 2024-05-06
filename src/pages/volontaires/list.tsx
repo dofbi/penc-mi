11525aca4f285a87b01672bda8f63f52a4663c52
Mamadou Diagne <genova@dofbi.com>
2024-05-06 22:44:49 +0000

    ♻️ refactor (pages): volontaires add search & fillter

--- src/pages/volontaires/list.tsx
+++ src/pages/volontaires/list.tsx
@@ -7,12 +7,15 @@ import {
     ShowButton,
     DeleteButton,
     ImageField,
+    FilterDropdown,
+    useSelect,
+    SaveButton,
 } from "@refinedev/antd";
-import { Table, Space } from "antd";
+import { Table, Space, Select, Form, Input, } from "antd";
 
 export const VolontaireList = () => {
     const translate = useTranslate();
-    const { tableProps } = useTable({
+    const { tableProps, searchFormProps } = useTable({
         syncWithLocation: true,
         sorters: {
         initial: [
@@ -25,10 +28,49 @@ export const VolontaireList = () => {
         meta: {
             select: "*, organisations(nom)",
         },
+        onSearch: (values: any) => {
+        return [
+            {
+            field: "full_name",
+            operator: "contains",
+            value: values?.full_name,
+            },
+        ];
+        },
+    });
+
+    const { selectProps: typesVolontaireSelectProps } = useSelect({
+        resource: "types_volontaire",
+        optionLabel: "nom",
+        optionValue: "nom",
+        sorters: [
+            {
+            field: "nom",
+            order: "asc",
+            },
+        ],
+    });
+
+    const { selectProps: organisationSelectProps } = useSelect({
+        resource: "organisations",
+        optionLabel: "nom",
+        optionValue: "id",
+        sorters: [
+            {
+            field: "nom",
+            order: "asc",
+            },
+        ],
     });
 
     return (
         <List>
+            <Form {...searchFormProps} layout="inline">
+                <Form.Item name="full_name">
+                <Input placeholder="Rechercher volontaire" />
+                </Form.Item>
+                <SaveButton hideText onClick={searchFormProps.form?.submit} />
+            </Form>
             <Table {...tableProps} rowKey="id">
                 <Table.Column
                     dataIndex="full_name"
@@ -47,11 +89,28 @@ export const VolontaireList = () => {
                 <Table.Column
                     dataIndex={["types_volontaire"]}
                     title={translate("Volontaires")}
+                    filterDropdown={(props) => (
+                    <FilterDropdown {...props}>
+                        <Select
+                            style={{ width: '150px' }}
+                            
+                            {...typesVolontaireSelectProps}/>
+                        </FilterDropdown>
+                    )}
                 />
                 <Table.Column
-                    dataIndex={["organisations"]}
+                    dataIndex={["organisation"]}
                     title={translate("Organisation")}
-                    render={(value) => (value?.nom)}
+                    render={(_, record: BaseRecord) => (record.organisations.nom)}
+                    filterDropdown={(props) => (
+                    <FilterDropdown {...props}>
+                        <Select
+                            showSearch
+                            style={{ width: 400 }}
+                            placeholder="Rechercher pour sélectionner"
+                            {...organisationSelectProps}/>
+                        </FilterDropdown>
+                    )}
                 />
                 <Table.Column
                     title={translate("table.actions")}
